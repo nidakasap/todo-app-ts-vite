aeda8cebc06a201cff49067693d40c84142a6a8a
Nida Kasap <nidakasapp@gmail.com>
2024-05-08 10:32:53 +0200

    Add axios for fetching todo data from mock API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,41 @@
 import {Container,Typography} from "@mui/material"
 import AddTodoComp from "../components/AddTodoComp"
 import TodoList from "../components/TodoList"
+import { useEffect, useState } from "react"
+import axios from "axios";
 
-const Home = () => {
+interface ITodoType {
+    isDone: boolean;
+    id: string | number;
+    task?: string; 
+  }
+
+const url = 'https://634ac3fc5df952851418480f.mockapi.io/api/todos'
+
+  const Home = () => {
+    const [todos,setTodos] = useState<ITodoType[]>([]);
+
+    const getTodos = async () => {
+        try {
+          const { data } = await axios<ITodoType[]>(url);
+          setTodos(data);         
+          console.log(data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      useEffect(() => {
+        getTodos();
+      }, []);
+
   return (
     <Container>
         <Typography align="center" color="error" variant="h5" component="h1">
             ToDo App with TypeScript
         </Typography>
         <AddTodoComp/>
-        <TodoList/>
+        <TodoList />
         </Container>
   )
 }
